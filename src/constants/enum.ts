a02cd1037a82db87d6c456752524fd9ef7e04ee0
KhiemmNguyen1 <133992148+KhiemmNguyen1@users.noreply.github.com>
2024-01-27 22:19:53 +0700

    create subdivision api

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -1,29 +1,6 @@
-export class AccountStatus {
-  public static readonly ACTIVE = 'ACTIVE';
-  public static readonly BAN = 'BANNED';
-}
-export class ProjectStatus {
-  public static readonly ACTIVE = 'ACTIVE';
-  public static readonly INACTIVE = 'INACTIVE';
-}
 export class SubdivisionStatus {
   public static readonly ACTIVE = 'ACTIVE';
   public static readonly INACTIVE = 'INACTIVE';
-}
-export class VillaStatus {
-  public static readonly ACTIVE = 'ACTIVE';
-  public static readonly INACTIVE = 'INACTIVE';
-  public static readonly SOLD = 'SOLD';
-}
-export class GenderType {
-  public static readonly MALE = 'MALE';
-  public static readonly FEMALE = 'FEMALE';
-  public static readonly OTHER = 'OTHER';
-}
-export class RoleName {
-  public static readonly ADMIN = 'ADMIN';
-  public static readonly USER = 'USER';
-  public static readonly STAFF = 'STAFF';
 }
 export enum TokenType {
   AccessToken,
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,10 +1,6 @@
 import { config } from 'dotenv';
 import { MongoClient, Db, Collection } from 'mongodb';
-import Project from '~/models/schemas/Project.schemas';
-import RefreshToken from '~/models/schemas/RefreshToken.schema';
 import Subdivision from '~/models/schemas/Subdivision.schemas';
-import User from '~/models/schemas/User.schemas';
-import Villa from '~/models/schemas/Villa.schemas';
 config();
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@theoasisluxury.vuzyle1.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,20 +21,8 @@ class DatabaseService {
       throw err;
     }
   }
-  get users(): Collection<User> {
-    return this.db.collection(process.env.DB_COLLECTION_USER as string);
-  }
-  get refreshTokens(): Collection<RefreshToken> {
-    return this.db.collection(process.env.DB_COLLECTION_REFRESHTOKEN as string);
-  }
-  get projects(): Collection<Project> {
-    return this.db.collection(process.env.DB_COLLECTION_PROJECT as string);
-  }
   get subdivisions(): Collection<Subdivision> {
     return this.db.collection(process.env.DB_COLLECTION_SUBDIVISION as string);
-  }
-  get villas(): Collection<Villa> {
-    return this.db.collection(process.env.DB_COLLECTION_VILLA as string);
   }
 }
 const databaseService = new DatabaseService();
