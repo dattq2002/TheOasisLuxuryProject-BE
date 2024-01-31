e601ece3bab1695efe0721708a770ada595a18d9
khang2905 <tvkhang2905@gmail.com>
2024-01-31 19:46:09 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-import usersRouter from './routes/users.routes';
+// import usersRouter from './routes/users.routes';
 // import YAML from 'yaml';
 // import fs from 'fs';
 // import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
-import databaseService from './services/database.services';
-import { defaultErrorHandler } from './middlewares/error.middlewares';
-import accountRouter from './routes/accounts.routes';
-import projectRouter from './routes/projects.routes';
-import subdivisionRouter from './routes/subdivisions.routes';
-import villaRouter from './routes/villas.routes';
+// import databaseService from './services/database.services';
+// import { defaultErrorHandler } from './middlewares/error.middlewares';
+// import accountRouter from './routes/accounts.routes';
+// import projectRouter from './routes/projects.routes';
+// import subdivisionRouter from './routes/subdivisions.routes';
+// import villaRouter from './routes/villas.routes';
 
 // const file = fs.readFileSync(path.resolve('the-oasis-luxury-api.yaml'), 'utf8');
 // const swaggerDocument = YAML.parse(file);
@@ -40,18 +40,18 @@ const app = express();
 app.use(express.json());
 const port = 5000;
 //kết nối database
-databaseService.connect();
+// databaseService.connect();
 app.get('/', (req, res) => {
   res.send('Welcome to OaSis Luxury API');
 });
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/accounts', accountRouter);
-app.use('/api/v1/projects', projectRouter);
-app.use('/api/v1/subdivisions', subdivisionRouter);
-app.use('/api/v1/villas', villaRouter);
+// app.use('/api/v1/users', usersRouter);
+// app.use('/api/v1/accounts', accountRouter);
+// app.use('/api/v1/projects', projectRouter);
+// app.use('/api/v1/subdivisions', subdivisionRouter);
+// app.use('/api/v1/villas', villaRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
-app.use(defaultErrorHandler);
+// app.use(defaultErrorHandler);
 app.listen(port, () => {
   console.log(`The OaSis Luxury này đang chạy swagger trên http://localhost:${port}/api-docs`);
 });
